191fe6aafdd40ccafd5d8445a327feb74193f939
Anirban Ganguly <88961372+mercdoggo01@users.noreply.github.com>
2023-01-14 15:34:30 +0530

    Add files via upload

---
+++ 5.) Functions.js
@@ -0,0 +1,23 @@
+// functions
+
+// 1.) function
+
+function add(a,b)
+{
+    return a+b;
+}
+
+let x = add(5,6);
+console.log(x);
+
+let y = add("Doggo","Mc-Choggo")
+console.log(y);
+
+// 2.) Arrow function
+// decreases syntax size introduced in ES6
+ subtract=(a,b)=>{
+    return a-b;
+}
+
+let z = subtract(10,9);
+console.log(z);
