dde38f66866f8fce688c8a7f366bac4cc90d61a4
Anirban Ganguly <88961372+mercdoggo01@users.noreply.github.com>
2023-01-14 13:57:14 +0530

    Add files via upload

---
+++ 2.) Prompt & Alert functions.js
@@ -0,0 +1,11 @@
+// 1.) Prompt and Alert functions
+
+let a = prompt("Hi");
+// will show a popup in browser
+console.log(typeof a);
+// prompt will convert any message in it to string
+
+// typecasting
+a = Number.parseInt(a);
+// 2.) converting string to number
+console.log(typeof a);
