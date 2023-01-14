3295fc2135d862790d56b37208ef5dbfc199e2ef
Anirban Ganguly <88961372+mercdoggo01@users.noreply.github.com>
2023-01-14 13:59:59 +0530

    Add files via upload

---
+++ 3.) Conditional Statement.js
@@ -0,0 +1,14 @@
+// 1.) Conditional statements
+let a = 69;
+if(a>69)
+{
+    console.log("Just missed");
+}
+else if(a<69)
+{
+    console.log("Either of you are short")
+}
+else
+{
+    console.log("Nice");
+}
